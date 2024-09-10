894b21caaa5293a74f458fa9d0a3911528bcc7aa
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-09-11 00:13:56 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,15 +6,36 @@ output "public_subnet_1_id" {
   value = aws_subnet.elastic_pub_subnet_1.id
 }
 
-output "public_subnet_2_id" {
-  value = aws_subnet.elastic_pub_subnet_2.id
-}
-
 output "private_subnet_1_id" {
   value = aws_subnet.elastic_priv_subnet_1.id
 }
 
 output "private_subnet_2_id" {
   value = aws_subnet.elastic_priv_subnet_2.id
+}
+
+# Outputs for EC2 instances
+output "instance_1_id" {
+  value = aws_instance.public_instance_1.id
+}
+
+output "instance_1_public_ip" {
+  value = aws_instance.public_instance_1.public_ip
+}
+
+output "instance_1_private_ip" {
+  value = aws_instance.private_instance_1.private_ip
+}
+
+output "instance_2_id" {
+  value = aws_instance.private_instance_1.id
+}
+
+output "instance_2_public_id" {
+  value = aws_instance.private_instance_2.id
+}
+
+output "instance_2_private_ip" {
+  value = aws_instance.private_instance_2.private_ip
 }
 
